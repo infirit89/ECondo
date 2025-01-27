7e94daaaa70d6224033d85270526766456816394
infirit89 <georgedautov808@gmail.com>
2025-01-27 15:59:21 +0200

    added application unittest project

---
+++ backend/ECondo.Domain/Users/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace ECondo.Domain.Users;
+
+public class Role
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = null!;
+    public string NormalizedName { get; set; } = null!;
+    public Guid ConcurrencyStamp { get; set; } = Guid.NewGuid();
+}
